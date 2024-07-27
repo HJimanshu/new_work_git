a84e1c67ad20744292a1fe9a5dca46c3bd66ff0d
HJimanshu <himsoni99@gmail.com>
2024-07-28 00:34:55 +0530

    change

--- Crud/crud_first/crud_app/models.py
+++ Crud/crud_first/crud_app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
-    username = models.CharField(max_length=50)
+    Firstname = models.CharField(max_length=50)
     Lastname = models.CharField(max_length=50)
     Email = models.EmailField(max_length=50)
     Contact = models.BigIntegerField()
